eb2fdcf7e3ac7de18df60058c3ffc4cf00c70d3f
Rhuan Menezes <rhuann@gmail.com>
2020-09-17 16:49:56 -0400

    Configurações externalizada: application.properties

--- src/main/java/io/github/rhuann/MyConfiguration.java
+++ src/main/java/io/github/rhuann/MyConfiguration.java
@@ -7,14 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyConfiguration {
 
-    @Bean
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
-
-    @Bean
-    public String applicationVersion(){
-        return "0.1";
-    }
 
 }
