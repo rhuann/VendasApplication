fac2e37ed98879bf98c774f4bdaf39de994b9c5d
Rhuan Menezes <rhuann@gmail.com>
2020-09-18 12:10:21 -0400

    Salvando e recuperando Clientes - usando JDBCTemplate

--- src/main/java/io/github/rhuann/VendasApplication.java
+++ src/main/java/io/github/rhuann/VendasApplication.java
@@ -1,10 +1,29 @@
 package io.github.rhuann;
 
+import io.github.rhuann.domain.entity.Cliente;
+import io.github.rhuann.domain.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired ClientesRepository clientesRepo){
+        return args -> {
+            clientesRepo.salvar(new Cliente("Rhuan"));
+            clientesRepo.salvar(new Cliente("Menezes"));
+
+            List<Cliente> todosClientes = clientesRepo.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
