50989cbe46c5108a1cf2ab8e6f2f425d4671c470
Rhuan Menezes <rhuann@gmail.com>
2020-09-18 10:16:51 -0400

    Criando annotations customizadas de configuração

---
+++ src/main/java/io/github/rhuann/Animal.java
@@ -0,0 +1,5 @@
+package io.github.rhuann;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- src/main/java/io/github/rhuann/MyConfiguration.java
+++ src/main/java/io/github/rhuann/MyConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
+@Development
 public class MyConfiguration {
 
 
