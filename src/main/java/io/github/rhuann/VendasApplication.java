21b5df8654cad9fdd41a11fe0ba346e93671c3ca
Rhuan Menezes <rhuann@gmail.com>
2020-09-22 11:19:29 -0400

    Requisição GET com parâmetros e Response Entity

--- src/main/java/io/github/rhuann/VendasApplication.java
+++ src/main/java/io/github/rhuann/VendasApplication.java
@@ -1,11 +1,23 @@
 package io.github.rhuann;
 
+import io.github.rhuann.domain.entity.Cliente;
+import io.github.rhuann.domain.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
 
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired ClientesRepository clientesRepository){
+        return args -> {
+            Cliente c = new Cliente(null, "Maria");
+            clientesRepository.save(c);
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
--- src/main/java/io/github/rhuann/rest/controller/ClienteController.java
+++ src/main/java/io/github/rhuann/rest/controller/ClienteController.java
@@ -1,22 +1,35 @@
 package io.github.rhuann.rest.controller;
 
+import io.github.rhuann.domain.entity.Cliente;
+import io.github.rhuann.domain.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(
-            value = "/hello/{nome}",
-            method = RequestMethod.GET
-    )
+    @Autowired
+    private ClientesRepository clientesRepository;
+
+    @GetMapping(value = "/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
-        return String.format("Hello %s", nomeCliente);
+    public ResponseEntity getClienteById(@PathVariable Integer id) {
+       Optional<Cliente> obj = clientesRepository.findById(id);
+        if (obj.isPresent()) {
+            return ResponseEntity.ok().body(obj.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
+
+
 
 }
