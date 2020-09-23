2f694c7ce7076440726453b88f0146db3392c895
Rhuan Menezes <rhuann@gmail.com>
2020-09-23 15:48:58 -0400

    Trabalhando com o modelo DTO

---
+++ src/main/java/io/github/rhuann/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,9 @@
+package io.github.rhuann.rest.dto;
+
+import javax.persistence.criteria.CriteriaBuilder;
+
+public class ItemPedidoDTO {
+
+    private Integer produto;
+    private Integer quantidade;
+}
---
+++ src/main/java/io/github/rhuann/rest/dto/PedidoDTO.java
@@ -0,0 +1,14 @@
+package io.github.rhuann.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PedidoDTO {
+
+    private Integer cliente;
+    private BigDecimal total;
+    private List<ItemPedidoDTO> itens;
+
+    
+
+}
